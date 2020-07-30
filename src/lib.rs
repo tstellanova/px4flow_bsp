c917913af17569f4500a026bdd954d5f651a86da
Todd Stellanova <tstellanova@users.noreply.github.com>
2020-07-30 16:25:10 -0700

    update for publication

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,10 @@ LICENSE: BSD3 (see LICENSE file)
 
 #![no_std]
 
+
+///! This crate provides some common useful interfaces to the
+///! PX4FLOW board, such as obtaining camera and gyro data.
+
 #[allow(unused)]
 pub mod peripherals;
 
